2f28f88ce40e7fcae58fef126bf2902936d395b5
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-01-05 14:30:57 -0600

    [FEATURE]: Add user creation, authentication, and protected view APIs
    - Created `CreateUserView` to handle user registration via POST request with `UserSerializer`.
    - Added `ProtectedView` that requires authentication using `IsAuthenticated` permission.
    - Implemented `TokenView` for issuing JWT access and refresh tokens upon successful authentication.
    - Improved error handling for missing or invalid credentials during authentication.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
+from .models import User
 
-# Create your views here.
+class CreateUserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully!"}, status=201)
+        return Response(serializer.errors, status=400)
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "This is a protected view!"})
+
+
+class TokenView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        if not email or not password:
+            return Response({"error": "Email and password are required"}, status=400)
+        
+        try:
+            user = User.objects.get(email=email)
+            
+            if user.check_password(password):
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'access': str(refresh.access_token),
+                    'refresh': str(refresh),
+                })
+            else:
+                return Response({"error": "Invalid credentials"}, status=400)
+
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=404)
